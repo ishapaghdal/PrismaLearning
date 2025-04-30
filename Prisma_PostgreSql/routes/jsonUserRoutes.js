2a0353ae0fbc829e56e43be5ce2213f2bac81c09
Preet_Abergine <preet@aubergine.co>
2025-04-30 11:13:50 +0530

    initial setup

--- Prisma_PostgreSql/routes/jsonUserRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createJsonUser, getAllJsonUser} from "../Controller/JsonUserController.js";
-
-const router = Router()
-
-router.post("/", createJsonUser);
-router.get("/" , getAllJsonUser)
-export default router;
